9386230de1430b625e83d22964d11fffa7d998ca
nofarbs90 <nofar.bs90@gmail.com>
2015-05-04 00:36:47 +0300

    add few things

--- ForumSystem/Member.cs
+++ ForumSystem/Member.cs
@@ -18,8 +18,10 @@ namespace ForumSystem
         private List<string> mySubForums;
         private List<string> myForums;
         //private double seniority;
-        private double timeLoggedIn;
+        public long TimeLoggedIn { get; set; }
         //private int numberOfMessagesLastYear;
+        public long NumberOfMessages { get; set; }
+        public int MemberType { get; set; }
 
         public Member() { }
 
@@ -39,128 +41,23 @@ namespace ForumSystem
                 {
                     Logger.logError("Failed to create a new member. Reason: email is empty");
                 }
+
             }
             else
             {
                 this.Username = username;
                 this.Password = password;
                 this.email = emailAddress;
-                this.timeLoggedIn = 0;
+                this.TimeLoggedIn = 0;
                 this.ID = IdGen.generateMemberId();
                 this.isActive = true;
                 this.myForums = new List<string>();
                 this.mySubForums = new List<string>();
                 this.myThreads = new List<string>();
                 this.myFriends = new List<string>();
+                this.MemberType = (int)Types.Regular;
                 Logger.logDebug(String.Format("A new user has been created. ID: {0} username: {1}, password: {2}, email: {3}", ID, Username, Password, emailAddress));
             }
-        }
-
-        //This method creates a sub-forum
-        public bool createSubForum(string title, string parent, List<string> moderators, int maxModerators)
-        {
-            ForumSystem forumSystem = ForumSystem.initForumSystem();
-            if ((String.IsNullOrEmpty(title)) || (String.IsNullOrEmpty(parent) || (moderators == null)))
-            {
-                if ((String.IsNullOrEmpty(title)))
-                {
-                    Logger.logError("Failed to create a new sub-forum. Reason: title is empty");
-                }
-
-                if ((String.IsNullOrEmpty(parent)))
-                {
-                    Logger.logError("Failed to create a new sub-forum. Reason: parent is empty");
-                }
-
-                if (moderators == null)
-                {
-                    Logger.logError("Failed to create a new sub-forum. Reason: moderators is null");
-                }
-
-                return false;
-            }
-            else
-            {
-                SubForum subForum = new SubForum(title, moderators, parent, maxModerators);
-                foreach (Forum forum in forumSystem.Forums.Values)
-                {
-                    if (string.Equals(forum.Title, parent))
-                    {
-                        forum.addSubForum(subForum);
-                        Logger.logDebug(String.Format("Sub forum has added succesfully. ID: {0}, title: {1}", subForum.ID, subForum.Title));
-                        return true;
-                    }
-                }
-                Logger.logError("Failed to add a new subform. Reason: parent can not be found");
-                return false;
-            }
-        }
-
-        //This method shows all discussions of a given sub-forum
-        public void viewDiscussions(string subForumName, string parent)
-        {
-            ForumSystem mainForum = ForumSystem.initForumSystem();
-            Forum forum;
-            SubForum subForum;
-            if (mainForum.Forums.ContainsKey(parent)) //&& 
-            {
-                forum = mainForum.Forums[parent];
-
-                if (forum.SubForums.ContainsKey(subForumName))
-                {
-                    subForum = forum.SubForums[subForumName];
-                    subForum.displayThreads();
-                }
-            }
-        }
-
-        //This method displays messages of a thread
-        public void viewMessages(string threadId, string subForumName, string parent)
-        {
-            ForumSystem mainForum = ForumSystem.initForumSystem();
-            if (mainForum.Forums.ContainsKey(parent))
-            {
-                Forum forum = mainForum.Forums[parent];
-                if (forum.SubForums.ContainsKey(subForumName))
-                {
-                    SubForum subForum = forum.SubForums[subForumName];
-                    Thread thread = subForum.Threads[threadId];
-
-                    thread.displayMessages();
-                }
-            }
-        }
-
-        //This method displays a message's replies
-        public string displayReplies(string forumName, string subForumName, string discussionTitle, int messageId)
-        {
-            StringBuilder sb = new StringBuilder();
-            ForumSystem mainForum = ForumSystem.initForumSystem();
-
-            if (mainForum.Forums.ContainsKey(forumName))
-            {
-                Forum forum = mainForum.Forums[forumName];
-                if (forum.SubForums.ContainsKey(subForumName))
-                {
-                    SubForum subForum = forum.SubForums[subForumName];
-                    if (subForum.Threads.ContainsKey(discussionTitle))
-                    {
-                        Thread thread = subForum.Threads[discussionTitle];
-                        foreach (Message message in thread.Messages)
-                        {
-                            if (messageId.Equals(message.ID))
-                            {
-                                foreach (Message reply in message.Replies)
-                                {
-                                    sb.Append(reply.displayMessage() + "\n");
-                                }
-                            }
-                        }
-                        return sb.ToString();
-                    }
-                }
-            }
-            return string.Empty;
         }
 
         public Forum enterForum(string forumName)
@@ -191,5 +88,26 @@ namespace ForumSystem
             }
             else return null;
         }
+
+        public void upgrade()
+        {
+            if (this.MemberType == (int)Types.Regular)
+            {
+                this.MemberType = (int)Types.Silver;
+            }
+
+            if (this.MemberType == (int)Types.Silver)
+            {
+                this.MemberType = (int)Types.Gold;
+            }
+
+        }
+
+        public void logout()
+        {
+            this.isActive = false;
+            this.TimeLoggedIn += DateTime.Now.Millisecond;
+        }
+
     }
 }
