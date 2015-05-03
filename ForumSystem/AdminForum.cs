1e2c2a38d5ebe00fd37cbfbd44e0412e65bbdda5
nofarbs90 <nofar.bs90@gmail.com>
2015-05-03 23:38:17 +0300

    add method "addSubForum" and the signature of "deleteSubForum" and upgrade\downgrade

--- ForumSystem/AdminForum.cs
+++ ForumSystem/AdminForum.cs
@@ -24,5 +24,38 @@ namespace ForumSystem
                 s.MaxModerators = moderatorNumber;
             }
         }
+
+        public void addSubForum(SubForum subForum)
+        {
+            if (subForum != null)
+            {
+                SubForums.Add(subForum.Title, subForum);
+                MemberSubForum msf = new MemberSubForum(subForum);
+                MemberSubForums.Add(subForum.Title, msf);
+                ModeratorSubForums.Add(subForum.Title, new ModeratorSubForum(msf));
+
+                Logger.logDebug(string.Format("The new sub forum: {0} has been created successfully with id {1}", subForum.Title, subForum.ID));
+            }
+            else
+            {
+                Logger.logError("Failed to add sub forum. Reason: sub forum is null");
+            }
+
+        }
+
+        public void deleteSubForum(string subForumID)
+        {
+            //TODO: add implementation
+        }
+
+        public void upgradeMemberID(string memberID)
+        {
+            //TODO: add implementation
+        }
+
+        public void downgraeMember(string memberID)
+        {
+            //TODO: add implementation
+        }
     }
 }
