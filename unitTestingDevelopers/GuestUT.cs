efdcf017537e0b27ca8db96940ba2a9e866d24e8
nofarbs90 <nofar.bs90@gmail.com>
2015-05-04 17:02:59 +0300

    add new unitTesting class

---
+++ unitTestingDevelopers/GuestUT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForumSystem;
+namespace unitTestingDevelopers
+{
+    [TestClass]
+    public class GuestUT
+    {
+        [TestMethod]
+        public void registerTest()
+        {
+            bool ans = false;
+            ForumSystem.ForumSystem system = ForumSystem.ForumSystem.initForumSystem();
+            Guest Nofar = new Guest();
+            Nofar.register("benshnof", "matanShoham", "benshnof@post.bgu.ac.il");
+            if (system.Members.ContainsKey("benshnof"))
+            {
+                ans = (system.Members["benshnof"] != null);
+            }
+            Assert.IsTrue(ans);
+        }
+    }
+}
