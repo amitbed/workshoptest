3831c5ca82114522e52b063211aaada51968b31c
Dean Abadie <dean89@gmail.com>
2015-05-05 15:57:01 +0300

    Query Update
    Query Update

--- ForumDB1/IQueries.cs
+++ ForumDB1/IQueries.cs
@@ -11,29 +11,29 @@ namespace ForumDB1
     {
        
         //Forums
-        Dictionary<string, Forum> dbGetForums();
-        void dbAddForum(Forum forum);
-        void dbRemoveForum(string forumID);
+        //Dictionary<string, Forum> dbGetForums();
+        //void dbAddForum(Forum forum);
+        //void dbRemoveForum(string forumID);
 
         //Sub forums
-        Dictionary<string, SubForum> dbGetSubForum();
-        void dbAddSubForum(SubForum subForum);
-        void dbRemoveSubForum(string subForumID); 
+        //Dictionary<string, SubForum> dbGetSubForum();
+        //void dbAddSubForum(SubForum subForum);
+        //void dbRemoveSubForum(string subForumID); 
 
         //Threads
-        Dictionary<string, Thread> dbGetThreads();
-        void dbAddThread(Thread thread);
-        void dbRemoveThread(string ThreadID); 
+        //Dictionary<string, Thread> dbGetThreads();
+        //void dbAddThread(Thread thread);
+        //void dbRemoveThread(string ThreadID); 
 
         //Messages
-        Dictionary<string, Message> dbGetMessages();
-        void dbAddMessage(Message message);
-        void dbRemoveMessage(string messageID);
+        //Dictionary<string, Message> dbGetMessages();
+        //void dbAddMessage(Message message);
+        //void dbRemoveMessage(string messageID);
 
         //Members
-        Dictionary<string, Member> dbGetMembers();              //This query retrieves all members from the DB
+        //Dictionary<string, Member> dbGetMembers();              //This query retrieves all members from the DB
         void dbAddMember(Member member);        //This query adds a new member to the DB
-        void dbRemoveMember(string MemberID);     //This query removes a member from the DB
+        //void dbRemoveMember(string MemberID);     //This query removes a member from the DB
         
     }
 }
