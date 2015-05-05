aadf86bbb720d617f6137713ce2536b88a27b257
Dean Abadie <dean89@gmail.com>
2015-05-05 14:20:50 +0300

    Migrations

--- ForumDB1/ForumSystemRepository.cs
+++ ForumDB1/ForumSystemRepository.cs
@@ -7,13 +7,21 @@ using ForumSystem;
 
 namespace ForumDB1
 {
-    class ForumSystemRepository
+    class ForumSystemRepository : IQueries
     {
         //This class contains methods that ask from the ForumDBContext to get data from the DB
         public List<Forum> getForums()
         {
             ForumDBContext fdbc = new ForumDBContext();
             return fdbc.Forums.ToList();
+        }
+
+        public List<Member> getMembers()
+        {
+            var context = new ForumDBContext();
+            var query = from member in context.Members select member;
+            var members = query.ToList();
+            return members;
         }
     }
 }
