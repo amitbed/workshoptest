07702f958a08145fc3ef2a9c66c36da89513121a
Amit Bedarshi <amitbed@post.bgu.ac.il>
2015-05-05 16:07:42 +0300

    booooozzzz

--- ForumDB1/Program.cs
+++ ForumDB1/Program.cs
@@ -11,16 +11,15 @@ namespace ForumDB1
     {
         static void Main(string[] args)
         {
-            using (var db = new ForumDBContext())
+          /*  using (var db = new ForumDBContext())
             {
                 var mem = new Member("Amit", "123456789", "amit@abc.com");
             //    var mem = new Member { Username = "Amit", Password = "123456789", Email = "amit@abc.com" };
                 db.Members.Add(mem);
                 db.SaveChanges();
-            }
-
+            }*/
             ForumSystemRepository repository = new ForumSystemRepository();
-
+            Member member = new Member("Shimon", "123456789", "li@gmail.com");
             
         }
     }
